c65c05fc218fc9505bd598ac9b3fa3625563dbfc
lhmd <2683235973@qq.com>
2023-05-27 16:14:46 +0800

    change require.sql

--- release/require.sql
+++ release/require.sql
@@ -1,7 +1,27 @@
 create database if not exists `online_book`;
 use online_book;
 
+drop table if exists `booking_history`;
+drop table if exists `plane`;
+drop table if exists `room`;
+drop table if exists `comments`;
 drop table if exists `hotel`;
+drop table if exists `user`;
+
+CREATE TABLE `user` (
+                        `user_id` INT NOT NULL AUTO_INCREMENT,
+                        `username` VARCHAR(63) NOT NULL,
+                        `password` VARCHAR(63) NOT NULL,
+                        `email` VARCHAR(63) NOT NULL,
+                        `phone` VARCHAR(63) NOT NULL,
+                        `gender` VARCHAR(63) NOT NULL,
+                        `birthday` DATE NOT NULL,
+                        `address` VARCHAR(63) NOT NULL,
+                        `credit` DECIMAL(8,2) NOT NULL DEFAULT 0.0,
+                        `is_admin` INT NOT NULL DEFAULT 0,
+                        PRIMARY KEY (`user_id`)
+);
+
 CREATE TABLE `hotel` (
                          `hotel_id` INT NOT NULL AUTO_INCREMENT,
                          `name` VARCHAR(63) NOT NULL,
@@ -15,7 +35,6 @@ CREATE TABLE `hotel` (
                          UNIQUE (`name`, `location`, `star_rating`)
 );
 
-drop table if exists `room`;
 CREATE TABLE `room` (
                         `hotel_id` INT NOT NULL,
                         `type` VARCHAR(63) NOT NULL,
@@ -25,7 +44,6 @@ CREATE TABLE `room` (
                         FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
 );
 
-drop table if exists `plane`;
 CREATE TABLE `plane` (
                          `plane_id` INT NOT NULL AUTO_INCREMENT,
                          `company` VARCHAR(63) NOT NULL,
@@ -39,19 +57,17 @@ CREATE TABLE `plane` (
                          UNIQUE KEY `unique_flight` (`company`, `start_time`, `end_time`, `start`, `end`)
 );
 
-drop table if exists `booking_history`;
 create table booking_history (
-                                 'user_id' int not null,
-                                 'state' int not null default 0,
-                                 'hotel_id' int not null default -1,
-                                 'type' varchar(63),
-                                 'plane_id' int not null default -1,
-                                 foreign key('user_id') references 'user'('user_id'),
-                                 foreign key('hotel_id') references 'hotel'('hotel_id'),
-                                 foreign key('plane_id') references 'plane'('plane_id')
+                                 user_id int not null,
+                                 state int not null default 0,
+                                 hotel_id int not null default -1,
+                                 type varchar(63),
+                                 plane_id int not null default -1,
+                                 foreign key(user_id) references `user`(user_id),
+                                 foreign key(hotel_id) references hotel(hotel_id),
+                                 foreign key(plane_id) references plane(plane_id)
 );
 
-drop table if exists `comments`;
 CREATE TABLE `comments` (
                             `user_id` INT NOT NULL,
                             `comment_time` DATETIME NOT NULL,
@@ -61,18 +77,3 @@ CREATE TABLE `comments` (
                             FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
                             FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
 );
-
-drop table if exists `user`;
-CREATE TABLE `user` (
-                        `user_id` INT NOT NULL AUTO_INCREMENT,
-                        `username` VARCHAR(63) NOT NULL,
-                        `password` VARCHAR(63) NOT NULL,
-                        `email` VARCHAR(63) NOT NULL,
-                        `phone` VARCHAR(63) NOT NULL,
-                        `gender` VARCHAR(63) NOT NULL,
-                        `birthday` DATE NOT NULL,
-                        `address` VARCHAR(63) NOT NULL,
-                        `credit` DECIMAL(8,2) NOT NULL DEFAULT 0.0,
-                        `is_admin` INT NOT NULL DEFAULT 0,
-                        PRIMARY KEY (`user_id`)
-)
