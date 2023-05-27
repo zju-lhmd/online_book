b4185744ce26f84a6975a59818f5830073028f19
Taizong1 <1252219871@qq.com>
2023-05-27 18:15:51 +0800

    plane done

---
+++ frontend/src/components/plane/plane_booking_history.ts
@@ -0,0 +1,56 @@
+import { reactive } from 'vue'
+
+//state 订单状态 0已预订 1已入住 2已退单 has_score是否已经评分
+interface Plane_booking_data{
+    order_no:string,
+    start_location1:string,
+    start_location2:string,
+    end_location1:string,
+    end_location2:string,
+    company1:string,
+    company2:string,
+    start_time1:Date,
+    start_time2:Date,
+    end_time1:Date,
+    end_time2:Date,
+    price:number,
+    state:number,
+}
+let order:Plane_booking_data[]=[];
+//测试用
+let date1=new Date()
+let date2=new Date()
+date1.setTime(date1.getTime() - 3600 * 1000 * 24*3)
+date2.setTime(date2.getTime() - 3600 * 1000 * 24*1)
+order.push({
+    order_no:"order1",
+    start_location1:"上海",
+    start_location2:"",
+    end_location1:"温州",
+    end_location2:"",
+    company1:"东方航空",
+    company2:"",
+    start_time1:date1,
+    start_time2:date1,
+    end_time1:date2,
+    end_time2:date2,
+    price:123,
+    state:0,
+});
+order.push({
+    order_no:"order1",
+    start_location1:"上海",
+    start_location2:"aa",
+    end_location1:"温州",
+    end_location2:"aa",
+    company1:"东方航空",
+    company2:"aa",
+    start_time1:date1,
+    start_time2:date1,
+    end_time1:date2,
+    end_time2:date2,
+    price:123,
+    state:0,
+});
+
+export let orders=reactive(order);
--- frontend/src/components/plane/plane_booking_history.vue
+++ frontend/src/components/plane/plane_booking_history.vue
@@ -1,11 +1,115 @@
 <template>
-航班预订历史查询界面
+    <el-breadcrumb separator=">" style="font-size: large;">
+        <el-breadcrumb-item :to="{ path: '/component/plane_booking_history' }">航班历史查询</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-col style="margin: 50px 100px 100px 100px;border-right: 1px solid #dadfe6;">
+        <li v-for="(order, key) in orders.slice((page - 1) * pageSize, page * pageSize)" :key="order.order_no"
+            class="list-item-target">
+            <el-row class="card-item-wrap">
+                <el-col class="left" :span="24">
+                    <el-row style="height: 35%;">
+                        <el-col :span="8" style="border:2px solid #dadfe6;font-size: 20px;">
+                            <div style="text-align: center;">订单号</div>
+                        </el-col>
+                        <el-col :span="16" style="border:2px solid #dadfe6;font-size: 20px;">
+                            <div style="text-align: center;">{{ order.order_no }}</div>
+                        </el-col>
+                    </el-row>
+                    <el-row style="height: 65%;">
+                        <el-col :span="14" style="border:2px solid #dadfe6;font-size: large;">
+                            <el-text style="color: black;margin: 0px 5px 0px 5px;">{{ order.company1 }}</el-text>
+                            <el-text style="color: black;margin: 0px 5px 0px 5px;">{{ order.start_location1 }}</el-text>
+                            <el-icon style="color: black;margin: 0px 5px 0px 5px;"><Right /></el-icon>
+                            <el-text style="color: black;">{{ order.end_location1 }}</el-text>
+                            <el-text style="color: black;margin: 0px 5px 0px 5px;">{{order.start_time1.toLocaleDateString()}}  {{order.start_time1.toLocaleTimeString()}}</el-text>
+                            <el-icon style="color: black;margin: 0px 5px 0px 5px;"><Right /></el-icon>
+                            <el-text style="color: black;margin: 0px 5px 0px 5px;">{{order.end_time1.toLocaleDateString()}}  {{order.end_time1.toLocaleTimeString()}}</el-text>
+                            <br />
+                            <div v-if="order.company2.length!=0">
+                                <el-text style="color: black;margin: 0px 5px 0px 5px;">{{ order.company2 }}</el-text>
+                                <el-text style="color: black;margin: 0px 5px 0px 5px;">{{ order.start_location2 }}</el-text>
+                                <el-icon style="color: black;margin: 0px 5px 0px 5px;"><Right /></el-icon>
+                                <el-text style="color: black;">{{ order.end_location2 }}</el-text>
+                                <el-text style="color: black;margin: 0px 5px 0px 5px;">{{order.start_time2.toLocaleDateString()}}  {{order.start_time2.toLocaleTimeString()}}</el-text>
+                                <el-icon style="color: black;margin: 0px 5px 0px 5px;"><Right /></el-icon>
+                                <el-text style="color: black;margin: 0px 5px 0px 5px;">{{order.end_time2.toLocaleDateString()}}  {{order.end_time2.toLocaleTimeString()}}</el-text>
+                            </div>
+                            
+                        </el-col>
+
+                        <el-col :span="5"
+                            style="border:2px solid #dadfe6;font-size: large;display: flex;justify-content: center;align-items: center;">
+                            <div v-if="order.state === 0">已预订</div>
+                            <el-test v-else-if="order.state === 1">已消费</el-test>
+                            <el-test v-else>已退订</el-test>
+                        </el-col>
+
+                        <el-col :span="5"
+                            style="border:2px solid #dadfe6;font-size: large;display: flex;justify-content: center;align-items: center;">
+                            <div>支付{{ Math.floor(order.price) }}元</div>
+                        </el-col>
+                    </el-row>
+                </el-col>
+
+            </el-row>
+        </li>
+        <el-pagination
+            style="padding: 10px 300px 10px 100px;"
+            layout=" prev, pager, next,jumper"
+            :current-page="page"
+            :page-size="pageSize"
+            :total="total"
+            :style="{'justify-content':'center'}"
+            @current-change="handleCurrentChange"
+            @size-change="handleSizeChange"/>
+    </el-col>
 </template>
-  
+
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import { orders } from "@/components/plane/plane_booking_history"
+const page = ref(1)
+const total = ref(orders.length)
+const pageSize = ref(4)
+//分页函数
+const handleSizeChange = (val: number) => {
+    pageSize.value = val;
+
+}
+const handleCurrentChange = (val: number) => {
+    page.value = val;
+}
+
 </script>
 
-<style scoped></style>
+<style scoped>
+li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+.card-item-wrap {
+    padding: 10px 300px 10px 100px;
+    background: #fff;
+    display: flex;
+    box-sizing: border-box;
+    width: 100%;
+    min-height: 120px;
+}
+
+.card-item-wrap .left {
+    border: 2px solid #dadfe6;
+}
+
+.card-item-wrap .right {
+    border: 2px solid #dadfe6;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
   
