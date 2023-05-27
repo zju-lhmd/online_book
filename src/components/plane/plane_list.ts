9154dabf02ebae161448310c7a959fc8e2e66691
Taizong1 <1252219871@qq.com>
2023-05-27 12:07:18 +0800

    a

---
+++ src/components/plane/plane_list.ts
@@ -0,0 +1,22 @@
+import { reactive } from 'vue'
+import { plane_search } from "@/components/plane/plane_search"
+//排序
+export const plane_sort = reactive({
+    //升序为false 降序为true
+    price_sort:false,
+    time_sort:false,
+    company_only:false,
+    direct_only:false
+})
+
+interface plane_data{
+    start_location:string,
+    end_location:string,
+    company:string,
+    start_time:Date,
+    end_time:Date,
+    price:number,
+    discount:number,
+}
+
+let a:number =1;
